54843f177f2ad8caa203b51d339d9b90025a6a6f
milenasbkn <66564278+milenasbkn@users.noreply.github.com>
2020-10-05 15:04:47 -0400

    Update Practice1.java

--- src/Testing/Practice1.java
+++ src/Testing/Practice1.java
@@ -3,5 +3,10 @@ package Testing;
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("Hello girl!");
+        
+        
+        // promena1, remote
+        
+        
     }
 }
